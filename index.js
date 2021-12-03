f4f78e395f33dee712d21ba988b40b287ce68389
marcelojoc <marcelojoc@gmail.com>
2021-12-03 15:20:21 -0300

    añadiendo rutas basicas

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ app.get("/", (req, res) =>{
   res.send("Hola mi server en Express");
 });
 
+app.get("/nueva", (req, res) =>{
+  res.send("Hola esta es una nueva ruta");
+});
+
+app.get("/products", (req, res) =>{
+  res.json({name: 'producto1',
+            precio : '343434'});
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
+
+
+
+
 app.listen(port, () =>{
   console.log("My port: " + port);
 });
